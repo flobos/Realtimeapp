7d782c07f0fc2ad5316e01960df5b18fca7c3e43
flobos <fernandolobos@gmail.com>
2019-10-21 15:47:11 -0300

    Token Exception Handle added

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,10 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -48,6 +52,21 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+
+        if ($exception instanceof TokenBlacklistedException){
+
+            
+            return response(['error' => 'Token Can not be used, get new one'], Response::HTTP_BAD_REQUEST);
+
+        }else if ($exception instanceof TokenInvalidException){
+
+            return response(['error' => 'Token is invalid'], Response::HTTP_BAD_REQUEST);
+        
+        }else if ($exception instanceof JWTException){
+
+            return response(['error' => 'Token is not provided'], Response::HTTP_BAD_REQUEST);
+        }
+
         return parent::render($request, $exception);
     }
 }
