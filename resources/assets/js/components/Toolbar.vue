e07b53878200d4845b2e5ca826207e6f0108d3b6
flobos <fernandolobos@gmail.com>
2019-10-22 00:03:08 -0300

    Vue integration added

---
+++ resources/assets/js/components/Toolbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-toolbar>
+    <v-toolbar-side-icon></v-toolbar-side-icon> 
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>Forum</v-btn>
+        <v-btn text>Ask Question</v-btn>
+        <v-btn text>Category</v-btn>
+        <v-btn text>Login</v-btn>
+      </v-toolbar-items>
+
+     
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+
+export default {
+   
+  
+}
+</script>
+
+<style>
+</style>
